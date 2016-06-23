003195fcc88b07d2ee1ced920ffd04bdc74dd62f
simone <simone@t450s>
2016-06-23 12:44:13 +0200

    First commit

---
+++ src/main/java/it/cnr/ilc/lc/omega/persistence/PersistenceHandler.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.cnr.ilc.lc.omega.persistence;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import sirius.kernel.di.std.Register;
+
+/**
+ *
+ *
+ */
+@Register(classes = PersistenceHandler.class)
+public class PersistenceHandler {
+
+    private EntityManagerFactory entityManagerFactory = null;
+
+    private static Logger log = LogManager.getLogger(PersistenceHandler.class);
+
+    public synchronized EntityManager getEntityManager() {
+
+        EntityManager manager = null;
+        log.info("entityManagerFactory is null? " + (null == entityManagerFactory));
+
+        if (null == entityManagerFactory) {
+            entityManagerFactory = Persistence.createEntityManagerFactory("OmegaPU");
+            log.info("entityManagerFactory is now open? " + entityManagerFactory.isOpen());
+        } else if (!entityManagerFactory.isOpen()) {
+            entityManagerFactory = Persistence.createEntityManagerFactory("OmegaPU");
+        }
+        try {
+            manager = entityManagerFactory.createEntityManager();
+        } catch (IllegalStateException e) {
+            throw new IllegalStateException(e);
+        }
+        return manager;
+
+    }
+
+    public synchronized void close() {
+
+        try {
+            if (null != entityManagerFactory) {
+                entityManagerFactory.close();
+                log.info("entityManagerFactory.close()");
+            } else {
+                log.warn("entityManagerFactory is null!");
+            }
+        } catch (IllegalStateException e) {
+            log.error("On close entityManagerFactory: " + e.getMessage());
+        }
+
+        log.info("entityManagerFactory closed");
+    }
+
+}
