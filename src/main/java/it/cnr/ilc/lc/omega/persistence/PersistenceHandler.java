7c0e9c7eb813b4d88a221e6d0ee2d4c27f45b1aa
simone <simone@t450s>
2018-06-07 16:01:46 +0200

    Prova di "mascheramento" delle eccezioni dovute alla creazione del DB senza eliminazione del vecchio

--- src/main/java/it/cnr/ilc/lc/omega/persistence/PersistenceHandler.java
+++ src/main/java/it/cnr/ilc/lc/omega/persistence/PersistenceHandler.java
@@ -8,10 +8,9 @@ package it.cnr.ilc.lc.omega.persistence;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
-import javax.persistence.TypedQuery;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.hibernate.tool.schema.spi.CommandAcceptanceException;
 import sirius.kernel.di.std.Register;
 
 /**
@@ -24,42 +23,44 @@ public class PersistenceHandler {
     //TODO http://stackoverflow.com/questions/14888040/java-an-entitymanager-object-in-a-multithread-environment
     private EntityManagerFactory entityManagerFactory = null;
     private ThreadLocal<EntityManager> threadLocal = null;
-
+    
     private static Logger log = LogManager.getLogger(PersistenceHandler.class);
-
+    
     {
         log.info("entityManagerFactory init()");
         try {
             entityManagerFactory = Persistence.createEntityManagerFactory("OmegaPU");
-
+            
+        } catch (CommandAcceptanceException cae) {
+            log.warn(cae.getMessage());
         } catch (Exception e) {
             log.error(e);
         }
         log.info("entityManagerFactory " + entityManagerFactory);
-
+        
     }
-
+    
     public synchronized EntityManager getEntityManager() {
-
+        
         log.info("threadLocal is " + threadLocal);
         if (null == threadLocal) {
             threadLocal = new ThreadLocal<>();
         }
         log.info("Tring to get the entityt manager...");
-
+        
         EntityManager em = threadLocal.get();
-
+        
         log.info("entityManagerFactory is null? " + (null == entityManagerFactory));
-
+        
         if (null == entityManagerFactory) {
             entityManagerFactory = Persistence.createEntityManagerFactory("OmegaPU");
             log.info("entityManagerFactory is now open? " + entityManagerFactory.isOpen());
         } else if (!entityManagerFactory.isOpen()) {
             entityManagerFactory = Persistence.createEntityManagerFactory("OmegaPU");
         }
-
+        
         try {
-
+            
             if (null == em || !em.isOpen()) {
                 em = entityManagerFactory.createEntityManager();
                 threadLocal.set(em);
@@ -69,12 +70,12 @@ public class PersistenceHandler {
             throw new IllegalStateException(e);
         }
         return em;
-
+        
     }
-
+    
     public synchronized void close() {
         log.info("Closing Entity Manager Factory...");
-
+        
         try {
             if (null != entityManagerFactory) {
                 if (entityManagerFactory.isOpen()) {
@@ -87,8 +88,8 @@ public class PersistenceHandler {
         } catch (IllegalStateException e) {
             log.error("On close() entityManagerFactory: " + e.getMessage());
         }
-
+        
         log.info("on close(): entityManagerFactory closed");
     }
-
+    
 }
